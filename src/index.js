ce64eb2a9528bfc96382ea784eeee8546a5bd572
SonGlad <okoshevy@gmail.com>
2024-03-10 20:58:23 +0200

    initial

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ root.render(
   <React.StrictMode>
     {/* <Provider store = {store}> */}
       {/* <PersistGate loading={null} persistor={{persistor}}> */}
-        <BrowserRouter basename='/react_template'>
+        <BrowserRouter basename='/neuralink'>
           <ThemeProvider theme={theme}>
             <GlobalStyle/>
             <App />
